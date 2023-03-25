6741e7a9ed406b70ecfb800a87c38a49cf40a2fc
engibaryan <xachik.engibar@gmail.com>
2023-03-25 22:38:49 +0400

    NewsApi working

--- app/src/main/java/com/example/retrofitlesson/retrofit/Products.kt
+++
@@ -1,7 +0,0 @@
-package com.example.retrofitlesson.retrofit
-
-data class Products(
-    val status: String,
-    val totalResults: Int,
-    val products: List<Product>
-)
--- app/src/main/java/com/example/retrofitlesson/retrofit/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.example.retrofitlesson.retrofit
-
-data class Source(
-    val id: String,
-    val name: String
-)
